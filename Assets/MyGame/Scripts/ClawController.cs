321bce28a397d5dc85b13cfdacd09d0dcdb298cf
Nicklas Ullrich <72389314+BeanutButler@users.noreply.github.com>
2022-05-19 14:15:54 +0200

    Claw Movement fully functional

--- Assets/MyGame/Scripts/ClawController.cs
+++ Assets/MyGame/Scripts/ClawController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ClawController : MonoBehaviour
 {
-    public enum Axis {x,z}
+    public enum Axis {x,y,z}
 
     public Transform target;
     [Range(0.1f, 2f)]
@@ -10,7 +10,9 @@ public class ClawController : MonoBehaviour
     [Range(0.1f, 2f)]
     public float deadZone = 0.2f;
     public Vector2 movementRange;
+    public float height;
     public Axis rotationAxis;
+
 
     private Vector3 startPos;
 
@@ -27,13 +29,14 @@ public class ClawController : MonoBehaviour
                 Move(transform.localRotation.x);
                 break;
             case Axis.z:
+            case Axis.y:
                 Move(transform.localRotation.z);
                 break;
         }
     }
 
     private void Move(float rotation)
-    {
+    { 
         if (rotation > -(deadZone / 2) && rotation < deadZone / 2) return;
 
         var fac = speed;
@@ -42,11 +45,20 @@ public class ClawController : MonoBehaviour
         switch (rotationAxis)
         {
             case Axis.x:
+                if(IsNotAtHome()) return;
                 target.position = new Vector3(target.position.x, target.position.y, Mathf.Clamp(target.position.z + (fac * Time.deltaTime), startPos.z -movementRange.x, startPos.z + movementRange.y));
                 break;
             case Axis.z:
+                if (IsNotAtHome()) return;
                 target.position = new Vector3(Mathf.Clamp(target.position.x - (fac * Time.deltaTime), startPos.x - movementRange.x, startPos.x + movementRange.y), target.position.y, target.position.z);
                 break;
+            case Axis.y:
+                target.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y + (fac * Time.deltaTime), startPos.y - height, startPos.y), target.position.z);
+                break;
         }
+    }
+    private bool IsNotAtHome()
+    {
+        return startPos.y != target.position.y;
     }
 }
